241b06de9415a48789d7e4a10d238bfe84529511
salcedo2 <samuelsalcedo400@gmail.com>
2025-01-12 20:30:19 -0500

    Add files via upload

---
+++ Include/footer.php
@@ -0,0 +1,77 @@
+<div class="container container-footer">
+    <div class="menu-footer">
+        <div class="contact-info">
+            <p class="title-footer">Información de Contacto</p>
+            <ul>
+                <li>
+                    Dirección: 71 Pennington Lane Vernon Rockville, CT
+                    06066
+                </li>
+                <li>Teléfono: 123-456-7890</li>
+                <li>Fax: 55555300</li>
+                <li>EmaiL: baristas@support.com</li>
+            </ul>
+            <div class="social-icons">
+                <span class="facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </span>
+                <span class="twitter">
+                    <i class="fa-brands fa-twitter"></i>
+                </span>
+                <span class="youtube">
+                    <i class="fa-brands fa-youtube"></i>
+                </span>
+                <span class="pinterest">
+                    <i class="fa-brands fa-pinterest-p"></i>
+                </span>
+                <span class="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                </span>
+            </div>
+        </div>
+
+        <div class="information">
+            <p class="title-footer">Información</p>
+            <ul>
+                <li><a href="#">Acerca de Nosotros</a></li>
+                <li><a href="#">Información Delivery</a></li>
+                <li><a href="#">Politicas de Privacidad</a></li>
+                <li><a href="#">Términos y condiciones</a></li>
+                <li><a href="#">Contactános</a></li>
+            </ul>
+        </div>
+
+        <div class="my-account">
+            <p class="title-footer">Mi cuenta</p>
+
+            <ul>
+                <li><a href="#">Mi cuenta</a></li>
+                <li><a href="#">Historial de ordenes</a></li>
+                <li><a href="#">Lista de deseos</a></li>
+                <li><a href="#">Boletín</a></li>
+                <li><a href="#">Reembolsos</a></li>
+            </ul>
+        </div>
+
+        <div class="newsletter">
+            <p class="title-footer">Boletín informativo</p>
+
+            <div class="content">
+                <p>
+                    Suscríbete a nuestros boletines ahora y mantente al
+                    día con nuevas colecciones y ofertas exclusivas.
+                </p>
+                <input type="email" placeholder="Ingresa el correo aquí...">
+                <button>Suscríbete</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <p>
+            Desarrollado por Programación para el mundo &copy; 2022
+        </p>
+
+        <img src="img/payment.png" alt="Pagos">
+    </div>
+</div>
---
+++ Include/header.php
@@ -0,0 +1,75 @@
+		<div class="container-hero">
+		    <div class="container hero">
+		        <div class="customer-support" >
+		            <div class="content-customer-support">
+		                <a href="index.php"><img src="img/logofinal.png" style="height: 5vw;"></img></a>
+		            </div>
+		        </div>
+
+		        <div class="container-logo">
+		            <h1 class="logo"><a href="#">Raíces Rabón</a></h1>
+		        </div>
+		        <div class="container-user">
+		            <i class="fa-solid fa-user"></i>
+		            <i class=""></i>
+		            <div class="content-shopping-cart">
+		                <span class="text"></span>
+		                <span class="number"></span>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+
+		<div class="container-navbar">
+		    <nav class="navbar">
+
+		        <label class="labe_hamburguesa" for="menu_hamburguesa">
+
+		            <i class="fa-solid fa-bars"> </i>
+
+
+		            <path fill-rule="evenodd"
+		                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+		        </label>
+		        <div class="search-form2">
+		            <form class="search-form">
+
+		                <input type="search" placeholder="Buscar..." />
+		                <button class="btn-search">
+		                    <i class="fa-solid fa-magnifying-glass"></i>
+		                </button>
+		            </form>
+		        </div>
+		        <input class="menu_hamburguesa" type="checkbox" name="" id="menu_hamburguesa">
+
+		        <ul class="ul_links menu">
+		            <li class="li_links">
+		                <a href="index.php" class="link">Inicio</a>
+		            </li>
+		            <li class="li_links">
+		                <a href="QuienesSomos.php" class="link">¿Quienes Somos?</a>
+		            </li>
+		            <li class="li_links">
+		                <a href="#" class="link">Historia</a>
+		            </li>
+		            <li class="li_links">
+		                <a href="Somos.php" class="link">Misión y Visión</a>
+		            </li>
+		            <li class="li_links">
+		                <a href="#Eventos" class="link">Eventos</a>
+		            </li>
+		            <li class="li_links">
+		                <a href="#Contacto" class="link">Contactános <i class="fa-solid fa-headset"></i></a>
+		            </li>
+
+		        </ul>
+		        <div class="search-form3">
+		            <form class="search-form">
+		                <input type="search" placeholder="Buscar..." />
+		                <button class="btn-search">
+		                    <i class="fa-solid fa-magnifying-glass"></i>
+		                </button>
+		            </form>
+		        </div>
+		    </nav>
+		</div>
